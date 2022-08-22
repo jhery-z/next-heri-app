bc7a0a8d0ee1a261b331794e3c6be4351b6fc042
inclip <backpackersjepang@gmail.com>
2022-08-23 05:22:09 +0700

    changes commit

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 
 const axiosConfig = {
-    baseURL: 'https://api.github.com',
+    baseURL: 'https://api.github.com/',
     auth: {
         username:process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
         password:process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET
     }
+//     baseURL: "https://api.github.com/users/jhery-z/repos",
+//     jsonp: true,
+//     method: "GET",
+//     dataType: "json",
+//     success: function(res) {
+//     console.log(res)
+//   }
 };
 
 function searchRepos(searchText, language) {
