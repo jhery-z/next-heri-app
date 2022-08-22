4d225561918b9106fa6ccd3f19348fef9f1471a5
inclip <backpackersjepang@gmail.com>
2022-08-23 05:39:42 +0700

    changes

---
+++ src/helpers/axios.helper.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const cancelConfig = {
+    request: null,
+    cancelToken: null
+}
+
+async function axiosGetCancellable(url, config) {
+
+}
+
+
+export {axiosGetCancellable};
