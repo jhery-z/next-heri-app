64f31fe1a061eb931f35964c527d6d474591d60b
jhery-z <herimaill4@gmail.com>
2022-08-25 00:16:27 +0700

    edit class name

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav class="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
+    <nav className="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
+  <div className="navbar-">
+    <a className="navbar-item" href="https://bulma.io">
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
     </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -17,16 +17,16 @@ const Navbar = () => {
   </div>
 
       
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
             <Link href="/">
-              <a class="navbar-item">Github Repos</a>
+              <a className="navbar-item">Github Repos</a>
             </Link>
             <Link href="/">
-              <a class="navbar-item">Home</a>
+              <a className="navbar-item">Home</a>
             </Link>
             <Link href="/about">
-              <a class="navbar-item">About</a>
+              <a className="navbar-item">About</a>
             </Link>
           </div>
         </div>
