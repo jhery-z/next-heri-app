ec2906dcfd6fc6855ae842d50c97137fd1f4580a
jhery-z <herimaill4@gmail.com>
2022-08-25 00:41:24 +0700

    add changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation">
   <div className="navbar-">
     <a className="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+      <Image src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
     </a>
 
     <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
